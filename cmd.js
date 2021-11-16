887c52cf0a0dbe0a1c56c98cd3b51aa0b78a6006
jjuanramos <jjuanramos91@gmail.com>
2021-11-16 18:33:51 +0100

    deleted helper

--- cmd.js
+++
@@ -1,4 +0,0 @@
-const fileNamePath = 'file:///home/juan/coding/learn/dbt/dbt-northwind-analytics/target/compiled/northwind/models/staging/stg_customers.sql';
-const compiledPath = '/home/juan/coding/learn/dbt/dbt-northwind-analytics/target/compiled/northwind/models/staging/stg_customers.sql';
-
-console.log(fileNamePath.includes(compiledPath));
